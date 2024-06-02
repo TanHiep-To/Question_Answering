27473581f3a860e5f3f7ab806d7704e47b581be6
Giang <21120237@student.hcmus.edu.vn>
2024-06-03 01:10:29 +0700

    modify UI

--- web/model.py
+++ web/model.py
@@ -3,7 +3,7 @@ from torch.nn.parameter import Parameter
 from torch.cuda.amp import GradScaler, autocast
 from torch.utils.data import Dataset, DataLoader
 from transformers import get_cosine_schedule_with_warmup
-from transformers import AutoConfig, AutoModelForQuestionAnswering,XLMRobertaForQuestionAnswering, XLMRobertaConfig
+from transformers import AutoConfig, AutoModelForQuestionAnswering,XLMRobertaForQuestionAnswering, XLMRobertaConfig, AutoTokenizer
 import streamlit as st
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
